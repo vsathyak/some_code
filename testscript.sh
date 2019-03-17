13353c042f181af53b3a8da8ab1879567c0c8674
vsathyak <vsathyak@gmail.com>
2019-03-17 17:00:55 -0400

    Updated the file to trigger the poll for A n A

--- testscript.sh
+++ testscript.sh
@@ -3,3 +3,4 @@ echo "this script is from github"
 NUMBER=$(($NUMBERONE + $NUMBERTWO))
 echo "$NUMBER"
 echo "Again checking the poll scm after moving the job under folder"
+echo "Adding new line to show the SCM polling for A & A"
